aad64ed6c6c068c9f4bf8ee2038e36bb332b4bdf
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-06-20 01:55:30 +0900

    Update Sample

--- position/app/src/main/java/jp/kaleidot725/sample/MainDialogFragment.kt
+++ position/app/src/main/java/jp/kaleidot725/sample/MainDialogFragment.kt
@@ -3,14 +3,31 @@ package jp.kaleidot725.sample
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
-class MainDialogFragment: DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return AlertDialog.Builder(requireActivity())
-                .setTitle(R.string.dialog_title)
-                .setPositiveButton(R.string.dialog_positive) { _, _ -> }
-                .setNegativeButton(R.string.dialog_negative) { _, _ -> }
-                .create()
+class MainDialogFragment(
+    private val gravity: Int,
+    private val verticalMargin: Float,
+    private val horizontalMargin: Float
+): DialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.dialog_fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adjustDialogPosition()
+    }
+
+    private fun adjustDialogPosition() {
+        dialog?.window?.attributes?.also { attributes ->
+            attributes.gravity = this.gravity
+            attributes.verticalMargin = this.verticalMargin
+            attributes.horizontalMargin = this.horizontalMargin
+        }
     }
 }
